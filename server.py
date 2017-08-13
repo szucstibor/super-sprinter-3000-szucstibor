611747aee999ec256ef662bd6931e465a5173c19
Szűcs Tibor <job.szucstibor@gmail.com>
2017-08-13 22:00:04 +0200

    Probably the final commit, close to being done but still far

--- server.py
+++ server.py
@@ -1,47 +1,61 @@
 from flask import Flask, render_template, redirect, request, session
 import csv
-from os import urandom
+import random
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/list")
+@app.route("/")
 def route_list():
-    with open('stories.csv', 'r') as story:
+    with open("stories.csv", "r") as story:
         reader = csv.reader(story)
         stories = [row for row in reader]
-    return render_template('list.html', stories=stories)
+    return render_template("list.html", stories=stories)
 
 
-@app.route('/story')
+@app.route("/story")
 def route_story():
     return render_template("form.html")
 
 
-@app.route('/save-story', methods=['POST'])
+@app.route("/save-story", methods=["POST"])
 def save():
     story = []
     table = get_table_from_file("stories.csv")
     print("POST request recieved!")
-    story.append(urandom(1))
-    story.append(request.form['title'])
-    story.append(request.form['story'])
-    story.append(request.form['criteria'])
-    story.append(request.form['business_value'])
-    story.append(request.form['estimation'])
-    story.append(request.form['status'])
-    with open('stories.csv', 'a') as stories:
-        writer = csv.writer(stories, delimiter=',')
+    story.append(random.randint(1, 1000000000))
+    story.append(request.form["title"])
+    story.append(request.form["story"])
+    story.append(request.form["criteria"])
+    story.append(request.form["business_value"])
+    story.append(request.form["estimation"])
+    story.append(request.form["status"])
+    with open("stories.csv", "a") as stories:
+        writer = csv.writer(stories, delimiter=",")
         writer.writerow(story)
     return redirect("/")
 
 
-@app.route('/delete')
-def delete():
+@app.route("/update/<id_of_story>", methods=["POST", "GET"])
+def update(id_of_story):
     table = get_table_from_file("stories.csv")
-    for record in table:
-        if record[0] == 1:
+    save_id = id_of_story
+    for record in range(len(table)):
+        if table[record][0] == id_of_story:
             table.pop(record)
-    return render_template("list.html")
+            print("ID found")
+    return redirect("/")
+
+
+@app.route("/delete/<id_of_story>", methods=["POST"])
+def delete(id_of_story):
+    table = get_table_from_file("stories.csv")
+    for record in range(len(table)):
+        if table[record][0] == id_of_story:
+            table.pop(record)
+            print("ID found")
+    write_table_to_file("stories.csv", table)
+    return redirect("/")
 
 
 def get_table_from_file(file_name):
@@ -50,6 +64,12 @@ def get_table_from_file(file_name):
         table = [element.replace("\n", "").split(";") for element in lines]
     return table
 
+
+def write_table_to_file(file_name, table):
+    with open(file_name, "wr") as file:
+        for record in table:
+            row = ";".join(record)
+            file.write(row + "\n")
 
 if __name__ == "__main__":
     app.secret_key = "Welcome_to_the_league_of_Draven"
