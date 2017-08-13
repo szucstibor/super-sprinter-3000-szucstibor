37c72c044d6940bfee780cb6dc56b78d1f19cdee
Szűcs Tibor <job.szucstibor@gmail.com>
2017-08-13 11:51:07 +0200

    requests working except the estimation

--- server.py
+++ server.py
@@ -5,15 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def route_index():
-    with open("stories.csv", "r") as stories:
-        reader = csv.reader(stories)
-        data_list = list(reader)
-    print(data_list)
-    return render_template('list.html', data_list=data_list)
+    return route_edit()
+
 
 @app.route('/story')
 def route_edit():
     return render_template("form.html")
+
+
+@app.route('/save-story', methods=['POST'])
+def save():
+    print("POST request recieved!")
+    title = request.form['title']
+    story = request.form['story']
+    acceptance = request.form['criteria']
+    business_value = request.form['business_value']
+    print("Business value working")
+    estimation = request.form['estimation']
+    print("estimation working")
+    status = request.form['status']
+    return "Oh hi, hello. Looks like everything works. But for how long?"
 
 
 if __name__ == "__main__":
