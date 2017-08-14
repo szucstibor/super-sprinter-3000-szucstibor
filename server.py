6f49caeb8b4f7b019ce5de435461239eec695cb1
Szűcs Tibor <job.szucstibor@gmail.com>
2017-08-14 09:15:24 +0200

    Late but delete is working

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, url_for
 import csv
 import random
 app = Flask(__name__)
+save_id = 0
 
 
 @app.route("/list")
 @app.route("/")
 def route_list():
-    with open("stories.csv", "r") as story:
-        reader = csv.reader(story)
-        stories = [row for row in reader]
+    stories = get_table_from_file("stories.csv")
     return render_template("list.html", stories=stories)
 
 
@@ -20,39 +19,51 @@ def route_story():
 
 @app.route("/save-story", methods=["POST"])
 def save():
+    global save_id
+    story_out = []
     story = []
     table = get_table_from_file("stories.csv")
     print("POST request recieved!")
-    story.append(random.randint(1, 1000000000))
-    story.append(request.form["title"])
-    story.append(request.form["story"])
-    story.append(request.form["criteria"])
-    story.append(request.form["business_value"])
-    story.append(request.form["estimation"])
-    story.append(request.form["status"])
-    with open("stories.csv", "a") as stories:
-        writer = csv.writer(stories, delimiter=",")
-        writer.writerow(story)
+    if save_id == 0:
+        story.append(str(random.randint(1, 1000000000)))
+    else:
+        story.append(save_id)
+        save_id = 0
+    story.append(str(request.form["title"]))
+    story.append(str(request.form["story"]))
+    story.append(str(request.form["criteria"]))
+    story.append(str(request.form["business_value"]))
+    story.append(str(request.form["estimation"]))
+    story.append(str(request.form["status"]))
+    table.append(story)
+    write_table_to_file("stories.csv", table)
     return redirect("/")
 
 
-@app.route("/update/<id_of_story>", methods=["POST", "GET"])
-def update(id_of_story):
+@app.route("/update/<id_of_story>", methods=["POST"])
+def update_record(id_of_story):
     table = get_table_from_file("stories.csv")
+    global save_id
     save_id = id_of_story
     for record in range(len(table)):
         if table[record][0] == id_of_story:
-            table.pop(record)
+            table.remove(record)
             print("ID found")
-    return redirect("/")
+            render_template("form.html")
+    return route_list()
 
 
-@app.route("/delete/<id_of_story>", methods=["POST"])
-def delete(id_of_story):
+@app.route("/delete", methods=["POST"])
+def delete():
+    print(request.form)
     table = get_table_from_file("stories.csv")
-    for record in range(len(table)):
-        if table[record][0] == id_of_story:
-            table.pop(record)
+    id_ = request.form
+    id_of_story = list(id_.keys())
+    print(id_of_story)
+    for record in table:
+        print(record)
+        if str(record[0]) == id_of_story[0]:
+            table.remove(record)
             print("ID found")
     write_table_to_file("stories.csv", table)
     return redirect("/")
@@ -61,14 +72,15 @@ def delete(id_of_story):
 def get_table_from_file(file_name):
     with open(file_name, "r") as file:
         lines = file.readlines()
-        table = [element.replace("\n", "").split(";") for element in lines]
+    table = [element.replace("\n", "").split(";") for element in lines]
     return table
 
 
 def write_table_to_file(file_name, table):
-    with open(file_name, "wr") as file:
+    print(table)
+    with open(file_name, "w") as file:
         for record in table:
-            row = ";".join(record)
+            row = ";".join(str(r) for r in record)
             file.write(row + "\n")
 
 if __name__ == "__main__":
